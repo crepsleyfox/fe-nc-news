e1622699a4c93db5a3d017cdf0c59eaa35b95e10
Your Namecrepsleyfox <you@example.comalexiscrepsleyfox@gmail.com>
2023-12-12 14:12:28 +0000

    jonathan requests for changes complete

--- src/1pages/LogInPage.jsx
+++ src/1pages/LogInPage.jsx
@@ -1,9 +1,10 @@
-import {UserList} from "../1components/Lists/UserList"
+import UserList from "../1components/Lists/UserList"
 export const LogInPage = () => {
 
 
     return (
         <div>
+            <h1>click your avatar to log in</h1>
             <UserList/>  
         </div>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { UserProvider } from './1components/UserContext'
 import { Routes, Route, useLocation} from 'react-router-dom'
-import UserList from './1components/Lists/UserList'
 import { Header } from './1components/Header'
 import { HomePage } from './1pages/HomePage'
 import { SplashPage } from './1pages/SplashPage'
+import { LogInPage } from './1pages/LogInPage'
 
 
 
@@ -20,7 +20,7 @@ const App = () => {
        {isHomePage && <Header />}
       <Routes>
         <Route path='/' element ={<SplashPage/>}/>
-        <Route path='/login' element ={<UserList/>}/>
+        <Route path='/login' element ={<LogInPage/>}/>
         <Route path='/main' element = {<HomePage/>}/>
       </Routes>
     </UserProvider>
